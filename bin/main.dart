ed81e1066cf7bfb65768edeb0db83c292596e608
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-11-10 16:07:50 -0500

    mapear body en una clase

--- bin/main.dart
+++ bin/main.dart
@@ -2,17 +2,20 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'package:paquetes/classes/reqres_respuesta.dart';
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 main(List<String> arguments) {
   final url = "https://reqres.in/api/users?page=2";
   http.get(url).then((res) {
 
-    final body = jsonDecode(res.body);
-    print(body);
-    print('page: ${body['page']}');
-    print('perpage: ${body['page']}');
-    print('page: ${body['data'][2]['id']}');
+    final resReqRes = reqResRespuestaFromJson(res.body);
+
+    // final body = jsonDecode(res.body);
+    // print(body);
+    print('page: ${resReqRes.page}');
+    print('perpage: ${resReqRes.perPage}');
+    print('page: ${resReqRes.data[2].id}');
     
   });
 }
