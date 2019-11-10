6ad3b3dbfaa1516df3a40f2dba9885f48526fd6d
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-11-10 15:43:29 -0500

    extraer body de peticiones http

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,13 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 
 main(List<String> arguments) {
   final url = "https://reqres.in/api/users?page=2";
-  http.get(url).then((resp) {
-    print(resp);
+  http.get(url).then((res) {
+
+    final body = jsonDecode(res.body);
+    print(body);
+    print('page: ${body['page']}');
+    print('perpage: ${body['page']}');
+    print('page: ${body['data'][2]['id']}');
+    
   });
 }
